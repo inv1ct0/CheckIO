9b8e9694f887cecb24499efce0e4c767a24c2d3f
inv1ct0 <a.m.yudenkov@yandex.ru>
2020-10-18 23:18:25 +0300

    elementary_chapter

--- Elementary/nearest_value.py
+++ Elementary/nearest_value.py
@@ -1,10 +1,6 @@
-import math
-
-
 def nearest_value(values: set, one: int) -> int:
     list_values = list(values)
     list_values.sort()
-    print(list_values)
     if one in values:
         return int(one)
     if int(one) == 0 and int(list_values[0]) > 0:
@@ -12,22 +8,20 @@ def nearest_value(values: set, one: int) -> int:
     if int(one) > int(list_values[len(list_values)-1]):
         return int(list_values[len(list_values)-1])
     for num in range(len(list_values) - 1):
-        if int(list_values[num - 1]) < one < int(list_values[num]):
-            if (one - math.fabs(int(list_values[num - 1]))) < (math.fabs(int(list_values[num])) - one):
-                return int(list_values[num - 1])
-            elif (one - math.fabs(int(list_values[num - 1]))) > (math.fabs(int(list_values[num])) - one):
+        if int(list_values[num]) < one < int(list_values[num+1]):
+            if (one - int(list_values[num])) < (int(list_values[num+1]) - one):
                 return int(list_values[num])
+            elif (one - int(list_values[num])) > (int(list_values[num+1]) - one):
+                return int(list_values[num+1])
             else:
-                return int(list_values[num - 1])
+                return int(list_values[num])
 
 
 if __name__ == '__main__':
     print("Example:")
-    # print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
-    print(nearest_value({0, -2}, -1))
-    # These "asserts" are used for self-checking and not for an auto-testing
-    assert nearest_value({0, -2}, -1) == -1
+    print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
 
+    # These "asserts" are used for self-checking and not for an auto-testing
     assert nearest_value({4, 7, 10, 11, 12, 17}, 9) == 10
     assert nearest_value({4, 7, 10, 11, 12, 17}, 8) == 7
     assert nearest_value({4, 8, 10, 11, 12, 17}, 9) == 8
